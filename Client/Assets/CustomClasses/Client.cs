f1c6541a815cc5d752090af51591b20d88831a08
Jose Morales <josfemova@localhost.localdomain>
2020-06-25 19:01:00 -0600

    add communication demo json files

---
+++ Client/Assets/CustomClasses/Client.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Net;
+using System.New.Sockets;
+using System.Text;
+public class Client{
+    private Client instance=null;
+
+    Client getInstance(){
+        if(instance == null){
+          return null;
+          // cambiar por inicializacion de cliente  
+        }
+        return this.instance;
+    }
+
+
+
+    finishLevel(){
+        CEList<int> log = new CEList<int>();
+    }
+    startGame(){
+
+    }
+}
---
+++ Client/Assets/CustomClasses/List.cs
@@ -0,0 +1,20 @@
+
+public class Node<T>
+{
+    Node<T> prev;
+    Node<T> next;
+}
+
+public class CEList<T>
+{
+    CEList()
+    {
+
+    }
+    Node<T> first;
+    Node<T> last;
+    public push_back(T x)
+    {
+
+    }
+}
