1fd993abf3f729219ed5184b0d2293ed1f6e7321
emanue19 <emarin702740530@gmail.com>
2020-07-02 20:11:08 -0600

    First test of A* Algorithm

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
-#include "include/list.hpp"
+#include "include/MoveGenerator.hpp"
+
 int main()
 {
-    std::cout<<"hello world";
-    ce::list<int> a;
-    a.push_back(1);
-    std::cout<<a.toString();
+    std::cout<<"Hello world"<<std::endl;
+
+    //Level matrix
+    int matrix[9][10] =
+            {
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
+                    { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+                    { 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+                    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+                    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 }
+            };
+
+    MoveGenerator moveGenerator(matrix);
+    Pair src = std::make_pair(8, 0); // Enemy position
+    Pair dest = std::make_pair(0, 0); // Player position
+
+    ce::list<Direction> path; // Shortest path from enemy position to player position
+    path = moveGenerator.getRoute(src, dest, RouteType::Astar);
+
     return 0;
 }
