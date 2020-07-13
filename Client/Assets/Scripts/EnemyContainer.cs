ef15639c8240fb008577a555a16bde3604d8cc5f
stphn <stephanie1117@hotmail.es>
2020-07-13 16:31:13 -0600

    enemy basic animation

--- Client/Assets/Scripts/EnemyContainer.cs
+++ Client/Assets/Scripts/EnemyContainer.cs
@@ -5,14 +5,39 @@ using UnityEngine;
 public class EnemyContainer : MonoBehaviour
 {
     public string enemyType;
+    public Animator animator;
+    private int orientationX = 0; //1 right, -1, left
+    private int orientationY = -1; //1 up, -1 down
+   
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        Vector3 change = Vector3.zero;
+        /*
+        myRigidbody.MovePosition(
+            transform.position
+        );
+        */
+
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (change != Vector3.zero)
+        {
+            orientationX = (int)change.x;
+            orientationY = (int)change.y;
+            animator.SetFloat("MoveX", change.x);
+            animator.SetFloat("MoveY", change.y);
+        }
+
+        
+       //anim.SetInt("MoveX", true);
         
     }
 }
