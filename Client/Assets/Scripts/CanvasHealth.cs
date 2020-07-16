6a86c85185d103f8f9ad44d2b97a8894241397ae
stphn <stephanie1117@hotmail.es>
2020-07-16 02:56:09 -0600

    health/score update

---
+++ Client/Assets/Scripts/CanvasHealth.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CanvasHealth : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    public int health;
+    public int numOfHearts;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+    void Start()
+    {   //exaple
+        numOfHearts = 4;
+        health = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        heartsPlayer(numOfHearts, health);
+    }
+/*
+numOfHearts = number of hearts, bc I don't remember
+health = number that change the number of hearts,
+ increase or decrease hearts
+
+*/
+    void heartsPlayer(int numOfHearts, int health){
+
+        for(int i = 0; i < hearts.Length; i++){
+            if(i < health){
+                hearts[i].sprite = fullHeart;
+            }else{
+                hearts[i].sprite = emptyHeart;
+            }
+            if(i < numOfHearts){
+                hearts[i].enabled = true;
+            }else{
+                hearts[i].enabled = false;
+            }
+        }
+    }
+    
+}
---
+++ Client/Assets/Scripts/CanvasScore.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasScore : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text scoreText;
+    public int score = 750;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        sumScore(score);
+        
+    }
+//1 hit, loss 1 live
+//1 found hearts, 1 more live
+   
+    
+
+    public void sumScore(int score){
+        scoreText.text = "score"+ score;
+    }
+}
